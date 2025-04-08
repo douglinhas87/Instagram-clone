394a075d1effc1359a930ea7b73b3ce8a6804aff
Douglas Ferraz <douglassfc02@gmail.com>
2025-04-08 15:33:12 -0300

    Criação dos elementos do feed

--- src/app/pages/feed/feed.component.ts
+++ src/app/pages/feed/feed.component.ts
@@ -1,23 +1,45 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-feed',
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './feed.component.html',
   styleUrls: ['./feed.component.css']
 })
 export class FeedComponent implements OnInit {
-  constructor(private router: Router) {}
+  posts = [
+    {
+      user: 'douglinhaass_',
+      image: 'https://pbs.twimg.com/media/GfwXpZIWIAA9RcQ?format=jpg&name=large',
+      description: 'Eu amo minha namorada',
+      liked: false,
+      likes: 172
+    },
+    {
+      user: 'ea.duque',
+      image: 'https://pbs.twimg.com/media/GOY0_LKWMAAvCJ8?format=jpg&name=large',
+      description: 'Eu e meu pAUrceiro fazendo bagunça',
+      liked: false,
+      likes: 98
+    }
+  ];
 
-  ngOnInit() {
+  ngOnInit(): void {
     const isLogged = localStorage.getItem('auth');
     if (!isLogged) {
-      this.router.navigate(['/']);
+      window.location.href = '/';
     }
   }
 
-  logout() {
+  logout(): void {
     localStorage.removeItem('auth');
-    this.router.navigate(['/']);
+    window.location.href = '/';
+  }
+
+  toggleLike(post: any): void {
+    post.liked = !post.liked;
+    post.likes += post.liked ? 1 : -1;
   }
 }
